d19a32ac4293f269ecbe23596ec5dc4e9002c0ec
SP23-BSE-026@cuiatk.edu.pk <134766259+AshtarNaqvi@users.noreply.github.com>
2026-04-09 11:18:04 +0500

    Initial commit for DevOps lab midterm

---
+++ script.js
@@ -0,0 +1,4 @@
+// Change the message when the button is clicked
+document.getElementById('changeBtn').addEventListener('click', () => {
+    document.getElementById('message').textContent = "You clicked the button! 🎉";
+});
